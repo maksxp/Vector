c13c189b9a95d8672a57c2b584132fbcb430d098
maksxp <maksym.murza@gmail.com>
2016-07-27 19:02:44 +0300

    Add files via upload

---
+++ Vector.java
@@ -0,0 +1,33 @@
+
+public class Vector {
+private double x;
+private double y;
+private double z;
+
+public Vector() {
+	super();
+}
+
+public Vector(double x, double y, double z) {
+	super();
+	this.x = x;
+	this.y = y;
+	this.z = z;
+}
+
+public static Vector sum (Vector a, Vector b){
+	return new Vector ((a.x+b.x),(a.y+b.y),(a.z+b.z));
+}
+public static double scalarMultiply (Vector a, Vector b){
+	return (a.x*b.x)+(a.y*b.y)+(a.z*b.z);
+}
+public static Vector vectorMultiply (Vector a, Vector b){
+	//works for right-oriented system of axes
+	return new Vector ((a.y*b.z-a.z*b.y),(a.z*b.x-a.x*b.z),(a.x*b.y-a.y*b.x));
+}
+
+@Override
+public String toString() {
+	return "Vector [x=" + x + ", y=" + y + ", z=" + z + "]";
+} 
+}
