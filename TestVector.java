e10c9ee6a4191331e88a26daf1cd947d863bb3c3
maksxp <maksym.murza@gmail.com>
2016-07-27 23:16:44 +0300

    Update TestVector.java

--- TestVector.java
+++ TestVector.java
@@ -3,11 +3,14 @@ public class TestVector {
 public static void main(String[] args) {
 	Vector a = new Vector(5,8,4); 
 	Vector b = new Vector(3,4,5); 
-	Vector cSum = Vector.sum(a,b);
+	Vector cSumStatic = Vector.sum(a,b);
+	Vector cSum = a.sum(b);
 	double cScalarMultiply = Vector.scalarMultiply(a,b);
 	Vector cVectorMultiply = Vector.vectorMultiply(a,b);
-	System.out.println("sum = "+ cSum);
+	System.out.println("sumStatic = "+ cSumStatic);
 	System.out.println("ScalarMultiply = "+ cScalarMultiply);
 	System.out.println("VectorMultiply = "+ cVectorMultiply);
+	//test of non-static method
+	System.out.println("sum = "+ cSum);
 }
 }
